e45226c74c6bfa9bd0d2df5b55a37a607aa54c4a
Rehman Ali <rehmanali001@gmail.com>
2019-07-08 20:52:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) == ""
+  if position_taken?
+  elsif position_taken? == ""
     false
   end
 end
